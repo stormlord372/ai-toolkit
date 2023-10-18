00de7c160c8d208fa1ef238efff17f37dd8d69b5
aiturria <96965@caf.local>
2023-10-18 15:52:46 +0200

    Red wine example using IDS

--- Cloud/airflow/dags/RedWine_MLOps_lifecycle.py
+++ Cloud/airflow/dags/RedWine_MLOps_lifecycle.py
@@ -25,8 +25,7 @@ from Data.read_data import read_data
 from Process.data_processing import data_processing
 from Models.ElasticNet_model_training import elasticNet_model_training
 from Models.SVC_model_training import svc_model_training
-from Deployment.Select_Best_Model import select_best_model
-# from Deployment.register_experiment import register_experiment
+from Deployment.register_experiment import register_experiment
 
 with DAG(
     'RedWine_MLOps_lifecycle', # Add the DAG name
@@ -58,14 +57,9 @@ with DAG(
         return svc_model_training(res)
     
     @task
-    def select_best_model_task():
-        print("Selection of the best model task:")
-        return select_best_model()
-    
-    # @task
-    # def register_experiment_task():
-    #     print("Register experiment task:")
-    #     return register_experiment()
+    def register_experiment_task():
+        print("Register experiment task:")
+        return register_experiment()
     
 
     # Instantiate each task and define task dependencies
@@ -73,8 +67,7 @@ with DAG(
     processing_result = data_processing_task(read_data_result)
     elasticNet_model_training_result = elasticNet_model_training_task(processing_result)
     svc_model_training_result = svc_model_training_result_task(processing_result)
-    select_best_model_result = select_best_model_task()
-    # register_experiment_result = register_experiment_task()
+    register_experiment_result = register_experiment_task()
 
     # Define the order of the pipeline
-    read_data_result >> processing_result >> [elasticNet_model_training_result, svc_model_training_result] >> select_best_model_result
+    read_data_result >> processing_result >> [elasticNet_model_training_result, svc_model_training_result] >> register_experiment_result
--- Cloud/airflow/src/redWine/Data/read_data.py
+++ Cloud/airflow/src/redWine/Data/read_data.py
@@ -6,6 +6,8 @@ scripts and included in the Data directory.
 """
 
 import pandas as pd
+from IDS_templates.rest_ids_consumer_connector import RestIDSConsumerConnector
+import config
 
 def read_data() -> pd.DataFrame:
     """
@@ -17,7 +19,14 @@ def read_data() -> pd.DataFrame:
         A Pandas DataFrame representing the content of the specified file.
     """
 
-    df = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv", sep = ';')
+    ids_consumer = RestIDSConsumerConnector()
+    data = ids_consumer.get_external_artifact_by_resource_title(
+        config.MLFLOW_EXPERIMENT, 
+        config.TRUE_CONNECTOR_EDGE_IP, 
+        config.TRUE_CONNECTOR_EDGE_PORT, 
+        config.TRUE_CONNECTOR_CLOUD_IP, 
+        config.TRUE_CONNECTOR_CLOUD_PORT
+    )
+    df = pd.read_csv(data, delimiter=';', quotechar='"')
     
-    print(df)
     return df
